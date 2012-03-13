1ff730a885b7ef610fc3c70bfee564a8d74cce46
Nicholas H.Tollervey <ntoll@ntoll.org>
2012-03-13 17:25:07 +0000

    Working constants.

---
+++ drogulus/dht/constants.py
@@ -0,0 +1,36 @@
+"""
+Defines constants used by the Kademlia DHT network.
+
+Copyright (C) 2012 Nicholas H.Tollervey.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+#: Represents the degree of parallelism in network calls.
+ALPHA = 3
+
+#: The maximum number of contacts stored in a k-bucket. Must be an even number.
+K = 20
+
+#: The timeout for network operations in seconds.
+RPC_TIMEOUT = 5
+
+#: The delay between iterations of node lookups in seconds.
+ITERATIVE_LOOKUP_DELAY = RPC_TIMEOUT / 2
+
+#: How long to wait before an unused k-bucket is refreshed in seconds.
+REFRESH_TIMEOUT = 3600 # 1 hour
+
+#: How long to wait before a node replicates any data it stores in seconds.
+REPLICATE_INTERVAL = REFRESH_TIMEOUT
--- test/dht/test_constants.py
+++ test/dht/test_constants.py
@@ -15,7 +15,7 @@ class TestConstants(unittest.TestCase):
         The alpha number represents the degree of parallelism in network calls.
         """
         self.assertIsInstance(constants.ALPHA, int,
-            "constant.ALPHA must be an integer.")
+            "constants.ALPHA must be an integer.")
 
     def test_K(self):
         """
@@ -24,7 +24,7 @@ class TestConstants(unittest.TestCase):
         number.
         """
         self.assertIsInstance(constants.K, int,
-            "constant.K must be an integer.")
+            "constants.K must be an integer.")
         self.assertEqual(0, constants.K % 2)
 
     def test_RPC_TIMEOUT(self):
@@ -32,29 +32,29 @@ class TestConstants(unittest.TestCase):
         The rpc timeout number defines the timeout for network operations in
         seconds.
         """
-        self.assertIsInstance(constant.RPC_TIMEOUT, int,
-            "constant.RPC_TIMEOUT must be an integer.")
+        self.assertIsInstance(constants.RPC_TIMEOUT, int,
+            "constants.RPC_TIMEOUT must be an integer.")
 
     def test_ITERATIVE_LOOKUP_DELAY(self):
         """
         The iterative lookup delay defines the delay (in seconds) between
         iterations of iterative node lookups for loose parallelism.
         """
-        self.assertIsInstance(constant.ITERATIVE_LOOKUP_DELAY, int,
-            "constant.ITERATIVE_LOOKUP_DELAY must be an integer.")
+        self.assertIsInstance(constants.ITERATIVE_LOOKUP_DELAY, int,
+            "constants.ITERATIVE_LOOKUP_DELAY must be an integer.")
 
     def test_REFRESH_TIMEOUT(self):
         """
         The refresh timeout defines how long to wait (in seconds) before an
         unused k-bucket is refreshed.
         """
-        self.assertIsInstance(constant.REFRESH_TIMEOUT, int,
-            "constant.REFRESH_TIMEOUT must be an integer.")
+        self.assertIsInstance(constants.REFRESH_TIMEOUT, int,
+            "constants.REFRESH_TIMEOUT must be an integer.")
 
     def test_REPLICATE_INTERVAL(self):
         """
         The replication interval defines how long to wait (in seconds) before a
         node replicates (republishes / refreshes) any data it stores.
         """
-        self.assertIsInstance(constant.REPLICATE_INTERVAL, int,
-            "constant.REPLICATE_INTERVAL must be an integer.")
+        self.assertIsInstance(constants.REPLICATE_INTERVAL, int,
+            "constants.REPLICATE_INTERVAL must be an integer.")
