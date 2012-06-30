e3904a43e88b53940bcdcfe64888866e6079dd81
Nicholas H.Tollervey <ntoll@ntoll.org>
2012-06-30 19:14:39 +0100

    Comment clean-up

--- drogulus/dht/constants.py
+++ drogulus/dht/constants.py
@@ -18,24 +18,24 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-#: Represents the degree of parallelism in network calls.
+# Represents the degree of parallelism in network calls.
 ALPHA = 3
 
-#: The maximum number of contacts stored in a k-bucket. Must be an even number.
+# The maximum number of contacts stored in a k-bucket. Must be an even number.
 K = 20
 
-#: The timeout for network operations in seconds.
+# The timeout for network operations in seconds.
 RPC_TIMEOUT = 5
 
-#: The delay between iterations of node lookups in seconds.
+# The delay between iterations of node lookups in seconds.
 ITERATIVE_LOOKUP_DELAY = RPC_TIMEOUT / 2
 
-#: How long to wait before an unused k-bucket is refreshed (in seconds).
+# How long to wait before an unused k-bucket is refreshed (in seconds).
 REFRESH_TIMEOUT = 3600 # 1 hour
 
-#: How long to wait before a node replicates any data it stores (in seconds).
+# How long to wait before a node replicates any data it stores (in seconds).
 REPLICATE_INTERVAL = REFRESH_TIMEOUT
 
-#: How long to wait before a node checks whether any buckets need refreshing or
-#: data needs republishing (in seconds).
+# How long to wait before a node checks whether any buckets need refreshing or
+# data needs republishing (in seconds).
 REFRESH_INTERVAL = REFRESH_TIMEOUT / 6 # Every 10 minutes.
