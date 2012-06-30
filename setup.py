7ea3fff037839cdb88b528b59b64c1e865c5cb71
Nicholas H.Tollervey <ntoll@ntoll.org>
2012-06-30 19:20:36 +0100

    Setup tidy up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from drogulus.version import get_version
 
 setup(
-    name='Drogulus',
+    name='drogulus',
     version=get_version(),
     description='A federated, decentralised, openly writable yet easily' +
         ' searchable information store and computation platform.',
@@ -11,13 +11,12 @@ setup(
     author='Nicholas H.Tollervey',
     author_email='ntoll@ntoll.org',
     url='http://packages.python.org/Drogulus',
-    packages=['drogulus'],
+    packages=['drogulus', 'drogulus/dht'],
     scripts=['bin/drogd'],
     license='GNU AGPLv3',
     classifiers=[
         'Development Status :: 1 - Planning',
         'Environment :: Console',
-        'Framework :: Twisted',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: GNU Affero General Public License v3',
@@ -25,5 +24,6 @@ setup(
         'Topic :: Database',
         'Topic :: Internet',
         'Topic :: Security',
+        'Topic :: System :: Distributed Computing',
     ]
 )
